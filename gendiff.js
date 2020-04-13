57361e83a4536190ecf41e63c0337bdde0b532f1
Mona Lisa <patapiks@mail.ru>
2020-04-13 22:11:29 +0300

    1/3

--- gendiff.js
+++ gendiff.js
@@ -9,9 +9,22 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(function (pathToFile2, pathToFile1) {
+  .action(function (pathToFile1, pathToFile2) {
+     const result = [];
      const object1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf8'));
      const object2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf8'));
-     console.log()
+     for (key in object1) {
+        if (object2.hasOwnProperty(key)) {
+           object2[key] === object1[key] ? 
+           1 + 1 : 
+           result.push(` +${key}: ${object2[key]}`)
+           result.push(`  ${key}: ${object1[key]}`)
+        }
+        if (!object2.hasOwnProperty(key)) result.push(` -${key}: ${object1[key]}`)
+     }
+     for (key in object2) {
+        if (!object1.hasOwnProperty(key)) result.push(` +${key}: ${object2[key]}`)
+     }
+     console.log(`{\n${result.join('\n')}\n}`)
    })
   .parse(process.argv);
