6f65e4f964eb3332d390b5b64e915f54eac49ec5
Sergey G <patapiks@mail.ru>
2020-04-21 16:45:47 +0300

    code works but refactoring is needed

--- src/buildingTreeDiff.js
+++ src/buildingTreeDiff.js
@@ -11,14 +11,13 @@ const buildingTreeDiff = (object1, object2) => {
 
     else if (Object.prototype.hasOwnProperty.call(object2, key)
     && object1[key] !== object2[key]) {
-      typeof (object1[key]) === 'object'
-        ? result.push(`- ${key}: `, buildingTreeDiff(object1[key], object1[key])) : result.push(`- ${key}: ${object1[key]}`);
-      typeof (object2[key]) === 'object'
-        ? result.push(`+ ${key}: `, buildingTreeDiff(object2[key], object2[key])) : result.push(`+ ${key}: ${object2[key]}`);
-    } else {
-      typeof (object1[key]) === 'object'
-        ? result.push(`- ${key}: `, buildingTreeDiff(object1[key], object1[key])) : result.push(`- ${key}: ${object1[key]}`);
-    }
+      if (typeof (object1[key]) !== 'object') result.push(`- ${key}: ${object1[key]}`);
+      else result.push(`- ${key}: `, buildingTreeDiff(object1[key], object1[key]));
+      if (typeof (object2[key]) !== 'object') result.push(`+ ${key}: ${object2[key]}`);
+      else result.push(`+ ${key}: `, buildingTreeDiff(object2[key], object2[key]));
+    } else if (typeof (object1[key]) !== 'object') result.push(`- ${key}: ${object1[key]}`);
+    else result.push(`- ${key}: `, buildingTreeDiff(object1[key], object1[key]));
+    return result;
   });
 
   Object.keys(object2).forEach((key) => {
