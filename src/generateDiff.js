a726cf1997f90e0aedb4f0a023bbb7baa618947f
Sergey G <patapiks@mail.ru>
2020-04-21 16:57:21 +0300

    code works but refactoring is needed

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -7,15 +7,16 @@ export default (pathToFile1, pathToFile2) => {
   const object2 = parse(pathToFile2);
 
   const tree = buildingTreeDiff(object1, object2);
-  const diff = tree.flat(2).reduce((acc, value) => {
-    if (value === '{') {
-      acc += `${value}`;
+  const diff = tree.flat(2).reduce((acc, currentValue) => {
+    let accum = acc;
+    if (currentValue === '{') {
+      accum += `${currentValue}`;
       count += 1;
-    } else if (value === '}') {
-      acc += `\n${'    '.repeat(count)}${value}`;
+    } else if (currentValue === '}') {
+      accum += `\n${'    '.repeat(count)}${currentValue}`;
       count -= 1;
-    } else acc += `\n  ${'    '.repeat(count)}${value}`;
-    return acc;
+    } else accum += `\n  ${'    '.repeat(count)}${currentValue}`;
+    return accum;
   });
 
   console.log(diff);
