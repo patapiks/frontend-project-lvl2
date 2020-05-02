9b5bd9916ed307142c9837e4d26cd6cb909c79b8
Mona Lisa <patapiks@mail.ru>
2020-05-02 21:40:15 +0300

    refactoring

--- src/buildingTreeDiff.js
+++ src/buildingTreeDiff.js
@@ -18,19 +18,13 @@ const buildingTreeDiff = (object1, object2) => {
       });
     }
     if (_.has(object2, key)
-    && object1[key] === object2[key]) {
-      tree.push({ name: key, status: 'unchanged', value: object1[key] });
-    }
-    if (!_.has(object2, key)) {
-      tree.push({ name: key, status: 'deleted', value: object1[key] });
-    }
+    && object1[key] === object2[key]) tree.push({ name: key, status: 'unchanged', value: object1[key] });
+    if (!_.has(object2, key)) tree.push({ name: key, status: 'deleted', value: object1[key] });
     return key;
   });
 
   Object.keys(object2).forEach((key) => {
-    if (!_.has(object1, key)) {
-      tree.push({ name: key, status: 'added', value: object2[key] });
-    }
+    if (!_.has(object1, key)) tree.push({ name: key, status: 'added', value: object2[key] });
   });
 
   return tree;
--- src/parsers.js
+++ src/parsers.js
@@ -4,10 +4,9 @@ import path from 'path';
 import yaml from 'js-yaml';
 
 export default (pathToFile) => {
-  const format = path.extname(pathToFile);
   const file = fs.readFileSync(pathToFile, 'utf8');
+  const format = path.extname(pathToFile);
   if (format === '.yml') return yaml.load(file);
-  if (format === '.json') return JSON.parse(file);
   if (format === '.ini') return ini.parse(file);
-  return null;
+  return JSON.parse(file);
 };
