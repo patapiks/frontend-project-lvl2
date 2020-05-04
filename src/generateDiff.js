2383a4e515a2650e9b9982e838e9972e0f5c4f35
Sergey G <patapiks@mail.ru>
2020-05-04 13:39:26 +0300

    side effect

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import parse from './parsers';
 import renderDefault from './formmaters/renderDefault';
 import renderPlain from './formmaters/renderPlain';
@@ -5,8 +6,10 @@ import renderJson from './formmaters/renderJson';
 import buildingTreeDiff from './buildingTreeDiff';
 
 export default (pathToFile1, pathToFile2, format) => {
-  const object1 = parse(pathToFile1);
-  const object2 = parse(pathToFile2);
+  const file1 = fs.readFileSync(pathToFile1, 'utf8');
+  const file2 = fs.readFileSync(pathToFile2, 'utf8');
+  const object1 = parse(file1, pathToFile1);
+  const object2 = parse(file2, pathToFile2);
 
   const tree = buildingTreeDiff(object1, object2);
   switch (format) {
--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,10 @@
-import fs from 'fs';
 import ini from 'ini';
 import path from 'path';
 import yaml from 'js-yaml';
 
-export default (pathToFile) => {
-  const file = fs.readFileSync(pathToFile, 'utf8');
-  const format = path.extname(pathToFile);
-  if (format === '.yml') return yaml.load(file);
-  if (format === '.ini') return ini.parse(file);
+export default (file, pathToFile) => {
+  const fileExtension = path.extname(pathToFile);
+  if (fileExtension === '.yml') return yaml.load(file);
+  if (fileExtension === '.ini') return ini.parse(file);
   return JSON.parse(file);
 };
