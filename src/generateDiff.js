510c363039d6d0b893e1e694b02f77549b4248b2
Sergey G <patapiks@mail.ru>
2020-05-12 12:30:30 +0300

    no files have data

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -6,10 +6,10 @@ import renderJson from './formmaters/renderJson';
 import buildTreeDiff from './buildTreeDiff';
 
 export default (pathToFile1, pathToFile2, format) => {
-  const file1 = fs.readFileSync(pathToFile1, 'utf8');
-  const file2 = fs.readFileSync(pathToFile2, 'utf8');
-  const object1 = parse(file1, pathToFile1);
-  const object2 = parse(file2, pathToFile2);
+  const data1 = fs.readFileSync(pathToFile1, 'utf8');
+  const data2 = fs.readFileSync(pathToFile2, 'utf8');
+  const object1 = parse(data1, pathToFile1);
+  const object2 = parse(data2, pathToFile2);
 
   const tree = buildTreeDiff(object1, object2);
   switch (format) {
