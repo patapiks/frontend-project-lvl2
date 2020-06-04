9b772ae2b58abaa53f1c7a500fed51c9ede1d27e
Sergey G <patapiks@mail.ru>
2020-06-04 13:23:31 +0300

    refactoring

--- __tests__/generateDiff.test.js
+++ __tests__/generateDiff.test.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import generateDiff from '../src/index';
 
-test.each([
-  ['.yml', '', 'exampleDefaultFormat'],
-  ['.ini', 'PLAIN', 'examplePlainFormat'],
-  ['.json', 'JSON', 'exampleJsonFormat.json'],
-])('GenerateDifference_test_1', (extension, format, example) => {
-  const getFixturePath = (fileName, ext = '') => path.join(__dirname, '..', '__fixtures__', fileName + ext);
+const getFixturePath = (fileName, ext = '') => path.join(__dirname, '..', '__fixtures__', `${fileName}${ext}`);
 
-  const actualValue = generateDiff(getFixturePath('before', extension), getFixturePath('after', extension), format);
+test.each([
+  ['yml', '', 'examplePrettyFormat'],
+  ['ini', 'PLAIN', 'examplePlainFormat'],
+  ['json', 'JSON', 'exampleJsonFormat.json'],
+])('GenerateDifference_test_1', (extension, format, example) => {
+  const actualValue = generateDiff(getFixturePath('before.', extension), getFixturePath('after.', extension), format);
   const expectedValue = fs.readFileSync(getFixturePath(example), 'UTF-8');
   expect(actualValue).toEqual(expectedValue);
 });
--- src/formmaters/renderPretty.js
+++ src/formmaters/renderPretty.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 
-const stringify = (value, indentSize) => {
+const stringify = (value, depth) => {
+  const indentSize = depth + 2;
   if (!_.isObject(value)) return value;
-  const result = Object.keys(value).map((key) => `${'  '.repeat(indentSize + 3)}${key}: ${value[key]}`);
-  return `{\n${result}\n${'  '.repeat(indentSize + 1)}}`;
+  const result = Object.keys(value).map((key) => `${'    '.repeat(indentSize)}${key}: ${value[key]}`);
+  return `{\n${result}\n${'    '.repeat(indentSize - 1)}}`;
 };
 
-const render = (tree, indentSize = 1) => {
+const render = (tree, depth = 0) => {
+  const indentSize = depth;
   const result = tree.map((node) => {
     const {
       name, value, beforeValue, afterValue, status, children,
@@ -14,20 +16,20 @@ const render = (tree, indentSize = 1) => {
 
     switch (status) {
       case 'deleted':
-        return `${'  '.repeat(indentSize)}- ${name}: ${stringify(value, indentSize)}`;
+        return `${'    '.repeat(indentSize)}  - ${name}: ${stringify(value, depth)}`;
       case 'added':
-        return `${'  '.repeat(indentSize)}+ ${name}: ${stringify(value, indentSize)}`;
+        return `${'    '.repeat(indentSize)}  + ${name}: ${stringify(value, depth)}`;
       case 'unchanged':
-        return `${'  '.repeat(indentSize)}  ${name}: ${stringify(value, indentSize)}`;
+        return `${'    '.repeat(indentSize)}    ${name}: ${stringify(value, depth)}`;
       case 'changed':
-        return `${'  '.repeat(indentSize)}+ ${name}: ${stringify(afterValue, indentSize)}\n${'  '.repeat(indentSize)}- ${name}: ${stringify(beforeValue, indentSize)}`;
+        return `${'    '.repeat(indentSize)}  + ${name}: ${stringify(afterValue, depth)}\n${'    '.repeat(indentSize)}  - ${name}: ${stringify(beforeValue, depth)}`;
       case 'complexValue':
-        return `${'  '.repeat(indentSize)}  ${name}: ${render(children, indentSize + 2)}`;
+        return `${'    '.repeat(indentSize)}    ${name}: ${render(children, depth + 1)}`;
       default:
         throw new Error(`Unknown status: '${status}'!`);
     }
   });
 
-  return `{\n${result.join('\n')}\n${'  '.repeat(indentSize - 1)}}`;
+  return `{\n${result.join('\n')}\n${'    '.repeat(indentSize)}}`;
 };
 export default render;
